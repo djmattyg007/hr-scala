614ec7df6097bf1e35a1495af77e70dc41d3e530
Matthew Gamble <git@matthewgamble.net>
2020-10-11 14:14:12 +1100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+hr-scala
+
+This is a port of hr to scala, as a learning exercise.
+
+You can find my C version of hr here:
+https://github.com/djmattyg007/hr
+
+This is a work in progress.
---
+++ src/main/scala/net/matthewgamble/hr/MainClass.scala
@@ -0,0 +1,42 @@
+package net.matthewgamble.hr
+
+import org.jline.terminal.TerminalBuilder
+
+/**
+ * @author ${user.name}
+ */
+object MainClass {
+  def main(args: Array[String]): Unit = {
+    // println("concat arguments = " + foo(args))
+
+    var width = 80
+    try {
+      val terminal = TerminalBuilder.terminal()
+      width = terminal.getWidth()
+    } catch {
+      case e: Throwable => 
+        width = 80
+    }
+
+    var lines = 1
+    if (args.length > 0) {
+      try {
+        lines = args(0).toInt
+      } catch {
+        case e: NumberFormatException =>
+          lines = 1
+      }
+    }
+
+    val outcharBuilder = new StringBuilder(width)
+    var x = 1
+    for (x <- 1 to width) {
+      outcharBuilder.append("#")
+    }
+
+    val outchar = outcharBuilder.toString
+    for (x <- 1 to lines) {
+      println(outchar)
+    }
+  }
+}
